d67211103242038c7980cf5ffa874f0210a1f17d
Justin Rhoads <rhoadsjustin@gmail.com>
2017-07-10 16:38:56 -0500

    Delete cosmic.js

--- config/cosmic.js
+++
@@ -1,8 +0,0 @@
-export default {
-  bucket: {
-    slug: 'jr-projects',
-    read_key: 'AfR8UTeXSIi7Xua8ALlvzC1YRpIVixpXTc2Px2lbE6rf6gMGur',
-    write_key: 'hp1rnr0JDN50du0B3IP6j0hf5vsR1Td5tigJOwHLYT4vRQro8H',
-    api_key: '16fd0e3b21db400295648d46afaf3fc2'
-  }
-}
